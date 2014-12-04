3a6ebbfb8b4e865144780e6fa713fc9dadc58d73
William Chou <wtchou712@gmail.com>
2014-12-03 20:19:31 -0600

    fixed margins, checks for empty inputs on category

--- public/js/index.js
+++ public/js/index.js
@@ -28,26 +28,37 @@ Parse.initialize("WSUgho0OtfVW9qimoeBAKW8qHKLAIs3SQqMs0HW6", "9ZmxN9S1vOOfTaL7lD
     var rounds = Math.ceil(Math.log(size)/Math.log(2));
     console.log(JSON.stringify(rounds));
 
-    //save the appropriate fields
-    var Brackets = Parse.Object.extend("Brackets");
-    var brackets = new Brackets();
-    brackets.set("furthest_round", 1);
-    brackets.set("total_rounds", rounds+1);
-    brackets.set("category", $("#category").val());
-    brackets.set("playerCount", parseInt($("#numPlayers").val()));
-    brackets.set("bracketSize", parseInt(size));
-    brackets.set("playersVoted", 0);
-    brackets.save(null, {
-    success: function(brackets) {
-      //alert('New object created with objectId: ' + ;
-      window.location.href = "./build.html?bid="+brackets.id + "&creator=yes";
-    },
-    error: function(brackets, error) {
-      // Execute any logic that should take place if the save fails.
-      // error is a Parse.Error with an error code and message.
-      alert('Failed to create new object, with error code: ' + error.message);
+    if ($("#category").val()==""){
+      alert("Enter a game title");
     }
-    });
+    else if ($("#numPlayers").val()==""){
+      alert("Enter number of players.");
+    }
+    else if (size=="nan"){
+      alert("Choose a bracket size.");
+    }
+    else {
+      //save the appropriate fields
+      var Brackets = Parse.Object.extend("Brackets");
+      var brackets = new Brackets();
+      brackets.set("furthest_round", 1);
+      brackets.set("total_rounds", rounds+1);
+      brackets.set("category", $("#category").val());
+      brackets.set("playerCount", parseInt($("#numPlayers").val()));
+      brackets.set("bracketSize", parseInt(size));
+      brackets.set("playersVoted", 0);
+      brackets.save(null, {
+      success: function(brackets) {
+        //alert('New object created with objectId: ' + ;
+        window.location.href = "./build.html?bid="+brackets.id + "&creator=yes";
+      },
+      error: function(brackets, error) {
+        // Execute any logic that should take place if the save fails.
+        // error is a Parse.Error with an error code and message.
+        alert('Failed to create new object, with error code: ' + error.message);
+      }
+      });
+    }
   });  
 });
 
